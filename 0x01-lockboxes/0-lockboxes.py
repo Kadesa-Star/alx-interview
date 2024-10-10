9185852f93b5435127357392ce0bb97a384e95ee
root <root@STAR>
2024-10-10 22:10:18 +0300

    lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,11 +3,11 @@
 Module to determine if all lockboxes can be unlocked.
 """
 
+
 def canUnlockAll(boxes):
     """
-    Determines if all boxes can be unlocked.
-    
-    :param boxes: List of lists where each sublist contains keys for other boxes.
+    Determines if all boxes can be unlocked
+    :param boxes: List of lists each sublist contains for keys boxes.
     :return: True if all boxes can be opened, False otherwise.
     """
     n = len(boxes)
@@ -15,7 +15,7 @@ def canUnlockAll(boxes):
     unlocked[0] = True
     keys = set(boxes[0])
     visited = set([0])
-    
+
     while keys:
         new_key = keys.pop()
         if new_key < n and new_key not in visited:
